d791b1b1894687e8a3cb063ec769b2cfb0bb9e44
subhanest <63142162+subhanest@users.noreply.github.com>
2022-06-04 21:39:44 +0500

    Delete csv.py

--- csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-jsondata = pd.read_json(r'C:\Users\DELL\Desktop\NCRA Task 1\english-words-master\words_dictionary.json', orient='index')
-jsondata.head()
-
-jsondata.to_csv('englishwords.csv')
-
-csvdata = pd.read_csv(r'englishwords.csv')
-csvdata.tail()
