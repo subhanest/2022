9191c5a1814e2847a3bd5d7b6c6d8e9298d42d2d
subhanest <63142162+subhanest@users.noreply.github.com>
2022-06-04 21:41:07 +0500

    Update csvf.py

--- csvf.py
+++ csvf.py
@@ -1,31 +1,32 @@
 import pandas as pd
 
-jsondata = pd.read_json(r'words_dictionary.json', orient='index')
-jsondata.head()
+def dict_call(word):
+    jsondata = pd.read_json(r'words_dictionary.json', orient='index')
+    jsondata.head()
 
-jsondata.to_csv('englishwords.csv')
+    jsondata.to_csv('englishwords.csv')
 
-csvdata = pd.read_csv(r'englishwords.csv')
+    csvdata = pd.read_csv(r'englishwords.csv')
 
-df = pd.DataFrame(csvdata)
-df2 = df.iloc[:,0]
-data_size = df2.size
+    df = pd.DataFrame(csvdata)
+    df2 = df.iloc[:,0]
+    data_size = df2.size
 
-def load_words():
-    with open(r'words_alpha.txt') as word_file:
-        valid_words = set(word_file.read().split())
-    return valid_words
+    def load_words():
+        with open(r'words_alpha.txt') as word_file:
+            valid_words = set(word_file.read().split())
+        return valid_words
 
-english_words = load_words()
+    english_words = load_words()
 
-word = input("Enter word to search: ") 
-word = word.lower()
+    #word = input("Enter word to search: ") 
+    word = word.lower()
 
-if (word in english_words):
-    for i in range(0,data_size):
-        if (df2.iloc[i] == word):
-            j = i;
-            print("Word "+ word+ " is present at " +str(j)+ " index of the dictionary")
-else:
-    print("Word " + word + " is not present in the dictionary")
-
+    if (word in english_words):
+        for i in range(0,data_size):
+            if (df2.iloc[i] == word):
+                j = i;
+                print("Word "+ word+ " is present at " +str(j)+ " index of the dictionary")
+                return word
+    else:
+        print("Word " + word + " is not present in the dictionary")
